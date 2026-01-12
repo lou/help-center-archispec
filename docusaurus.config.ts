7e102999724ad443e2f2d0c6e7dd5ac2f2113026
lou <louiscuny@gmail.com>
2026-01-12 17:26:22 +0100

    Fix baseUrl

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
 	url: "https://lou.github.io",
 	// Set the /<baseUrl>/ pathname under which your site is served
 	// For GitHub pages deployment, it is often '/<projectName>/'
-	baseUrl: "/help-center-archispec/",
+	baseUrl: "/",
 
 	// GitHub pages deployment config.
 	// If you aren't using GitHub pages, you don't need these.
