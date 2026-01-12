a4047fbd8bebddbdc3aa460ca991932c69b27a95
lou <louiscuny@gmail.com>
2026-01-12 14:54:47 +0100

    Fix deploy

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,95 +5,96 @@ import { themes as prismThemes } from "prism-react-renderer";
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: "Centre d'aide Archispec",
-  tagline: "Centre d'aide Archispec",
-  favicon: "img/favicon.ico",
+	title: "Centre d'aide Archispec",
+	tagline: "Centre d'aide Archispec",
+	favicon: "img/favicon.ico",
 
-  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
-  future: {
-    v4: true, // Improve compatibility with the upcoming Docusaurus v4
-  },
+	// Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
+	future: {
+		v4: true, // Improve compatibility with the upcoming Docusaurus v4
+	},
 
-  // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+	// Set the production url of your site here
+	url: "https://lou.github.io",
+	// Set the /<baseUrl>/ pathname under which your site is served
+	// For GitHub pages deployment, it is often '/<projectName>/'
+	baseUrl: "/help-center-archispec/",
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+	// GitHub pages deployment config.
+	// If you aren't using GitHub pages, you don't need these.
+	organizationName: "lou", // Usually your GitHub org/user name.
+	projectName: "help-center-archispec", // Usually your repo name.
+	trailingSlash: false,
 
-  onBrokenLinks: "throw",
+	onBrokenLinks: "throw",
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: "fr",
-    locales: ["fr"],
-  },
+	// Even if you don't use internationalization, you can use this field to set
+	// useful metadata like html lang. For example, if your site is Chinese, you
+	// may want to replace "en" with "zh-Hans".
+	i18n: {
+		defaultLocale: "fr",
+		locales: ["fr"],
+	},
 
-  themes: ["docusaurus-theme-search-typesense"],
+	themes: ["docusaurus-theme-search-typesense"],
 
-  presets: [
-    [
-      "@docusaurus/preset-classic",
-      {
-        docs: {
-          sidebarPath: "./sidebars.ts",
-          routeBasePath: "/",
-        },
-        blog: false,
-        theme: {
-          customCss: "./src/css/custom.css",
-        },
-      } satisfies Preset.Options,
-    ],
-  ],
+	presets: [
+		[
+			"@docusaurus/preset-classic",
+			{
+				docs: {
+					sidebarPath: "./sidebars.ts",
+					routeBasePath: "/",
+				},
+				blog: false,
+				theme: {
+					customCss: "./src/css/custom.css",
+				},
+			} satisfies Preset.Options,
+		],
+	],
 
-  themeConfig: {
-    typesense: {
-      typesenseServerConfig: {
-        nodes: [
-          {
-            host: "typesense-railway-production-c959.up.railway.app",
-            port: 443,
-            protocol: "https",
-          },
-        ],
-        apiKey: "82n8y7slc32p64tnaij71h23ao52n9mp",
-      },
-      typesenseCollectionName: "docusaurus-2",
-    },
-    // Replace with your project's social card
-    colorMode: {
-      respectPrefersColorScheme: false,
-      disableSwitch: true,
-      defaultMode: "light",
-    },
-    navbar: {
-      title: "Centre d'aide",
-      logo: {
-        alt: "My Site Logo",
-        src: "img/logo.png",
-      },
-      items: [],
-    },
-    prism: {
-      theme: prismThemes.github,
-      darkTheme: prismThemes.dracula,
-    },
-    plugins: [
-      [
-        "@docusaurus/plugin-sitemap",
-        {
-          // plugin options
-        },
-      ],
-    ],
-  } satisfies Preset.ThemeConfig,
+	themeConfig: {
+		typesense: {
+			typesenseServerConfig: {
+				nodes: [
+					{
+						host: "typesense-railway-production-c959.up.railway.app",
+						port: 443,
+						protocol: "https",
+					},
+				],
+				apiKey: "82n8y7slc32p64tnaij71h23ao52n9mp",
+			},
+			typesenseCollectionName: "docusaurus-2",
+		},
+		// Replace with your project's social card
+		colorMode: {
+			respectPrefersColorScheme: false,
+			disableSwitch: true,
+			defaultMode: "light",
+		},
+		navbar: {
+			title: "Centre d'aide",
+			logo: {
+				alt: "My Site Logo",
+				src: "img/logo.png",
+			},
+			items: [],
+		},
+		prism: {
+			theme: prismThemes.github,
+			darkTheme: prismThemes.dracula,
+		},
+		plugins: [
+			[
+				"@docusaurus/plugin-sitemap",
+				{
+					// plugin options
+				},
+			],
+		],
+	} satisfies Preset.ThemeConfig,
 };
 
 export default config;
